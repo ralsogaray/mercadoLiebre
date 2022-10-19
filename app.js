d79cfdc3324420ce29404c396a7e9d094465ae26
ralsogaray <rodrigo.alsogaray91@gmail.com>
2022-10-19 20:10:20 -0300

    Se agrega puerto para render

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const app = express()
 // import path for using absolutes routes
 const path = require('path')
 
+// dynamic port for render
+const port = process.env.PORT || 3000
 
 // using this to tell to express the route for using the public resources 
 app.use(express.static(path.resolve( 'public' ) ) )
 
 // starting server on port 3000
-app.listen(3000, () =>{
+app.listen(port, () =>{
     console.log("Server up, running in port 3000")
 })
 
